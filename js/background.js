7279e55cd60ba8414b641cbe0efd949a9cd0c2fe
senhee Lee <senhee.lee@kr.ey.com>
2022-11-06 01:29:00 +0900

    image add

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 const bgImg = document.getElementById('bgImg');
-const bgImages = ["bali03.jpg", "bali07.jpg", "hawaii01.jpg"];
+const bgImages = ["bali03.jpg", "bali07.jpg", "hawaii01.jpg", "japan02.jpg", "japan05.jpg"];
 
 const randomImg = bgImages[Math.floor(Math.random() * bgImages.length)];
-bgImg.src = `http://harunao.net/wp-content/uploads/2014/05/${randomImg}`
+bgImg.src = `img/${randomImg}`
