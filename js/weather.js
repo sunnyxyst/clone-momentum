c5f6e6d186ac14cb045787417401277e3d73ae55
senhee Lee <senhee.lee@kr.ey.com>
2022-11-06 18:56:42 +0900

    add ap api

--- js/weather.js
+++ js/weather.js
@@ -4,6 +4,7 @@ function geoOn(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const apUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
     fetch(url)
         .then(response => response.json()) // json 형태로 data fetch
         .then((data) => {   // data 변수에 ojb형태로 data 담기
@@ -16,12 +17,42 @@ function geoOn(position) {
             weatherInfo.innerText = data.weather[0].main;
             locationInfo.innerText = data.name;
             weatherInfo.append(weatherIcon);
-            //console.log(weatherIcon)
+            //console.log(data)
+        });
+    fetch(apUrl)
+        .then(response => response.json())
+        .then((apData) => {
+            const airPm2 = document.querySelector('.ap2to5');
+            const airPm10 = document.querySelector('.ap10')
+            
+            const pm2Level = apData.list[0].components.pm2_5;
+            const pm10Level = apData.list[0].components.pm10;
+            
+            if (pm2Level <= 15) {
+                //console.log('good')
+                airPm2.classList.add('best');
+            } else if (15 < pm2Level <= 35) {
+                airPm2.classList.add('normal');
+            } else if (35 < pm2Level <= 75) {                
+                airPm2.classList.add('bad');
+            } else {
+                airPm2.classList.add('worst');
+            }
+            
+            if (pm10Level <=30) {
+                airPm10.classList.add('worst');
+            } else if (30 < pm10Level <=80) {
+                airPm10.classList.add('normal');
+            } else if (80 < pm10Level <= 150) {
+                airPm10.classList.add('bad');
+            } else {
+                airPm10.classList.add('worst');
+            }
         });
 }
 
 function geoOff() {
-    console.log('CANNOT FIND YOUR LOCATION');
+   alert('CANNOT FIND YOUR LOCATION');
 }
 
 navigator.geolocation.getCurrentPosition(geoOn, geoOff);                                                                       
